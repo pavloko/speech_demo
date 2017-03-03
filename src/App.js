3ce3a8aafb9ea15a2e27eeaa94740fbd4bef4085
pavloko <pavlo.kochubei@hotmail.com>
2017-03-03 03:17:07 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import React, {Component} from 'react';
+import './App.css';
+//noinspection JSUnresolvedVariable
+import mic from './mic.svg';
+//noinspection JSUnresolvedVariable
+import success from './success.svg';
+//noinspection JSUnresolvedVariable
+import fail from './fail.svg';
+//noinspection JSUnresolvedVariable
+import play from './play.svg';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sentence: 'How do you do?',
+      isListeningToUser: false,
+      isExerciseCorrect: false,
+      isExerciseWrong: false,
+    };
+  }
+
+  handleSpeechResult = (event) => {
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        let userSpeech = event.results[i][0].transcript;
+        let controlSentenceTrimmed = this.state.sentence.toLowerCase().replace(/[?.]/, '');
+        if (userSpeech === controlSentenceTrimmed) {
+          this.recognition.stop();
+          this.setState({
+            isExerciseCorrect: true,
+            isListeningToUser: false,
+          });
+        } else {
+          this.recognition.stop();
+          this.setState({
+            isExerciseWrong: true,
+            isListeningToUser: false,
+          });
+        }
+      }
+    }
+  };
+
+  componentDidMount() {
+    if (!('webkitSpeechRecognition' in window)) {
+      console.warn('No Speech API');
+    } else {
+      this.recognition = new window.webkitSpeechRecognition();
+      this.recognition.lang = 'en-US';
+      // this.recognition.continuous = true; // keep listening when the user stopped talking
+      // this.recognition.interimResults = true; // recognition results might change
+      this.recognition.onresult = this.handleSpeechResult;
+    }
+  }
+
+  listenToUser = () => {
+    if (this.state.isListeningToUser) {
+      this.recognition.stop();
+      this.setState({
+        isListeningToUser: false
+      });
+      return;
+    }
+
+    this.recognition.start();
+    this.setState({
+      isListeningToUser: true
+    });
+  };
+
+  playSentence = () => {
+    let synth = window.speechSynthesis;
+    let sentenceToSpeak = new SpeechSynthesisUtterance(this.state.sentence);
+    sentenceToSpeak.lang = 'en-GB'; // Sounds much better than en-US, need to look into SpeechSynthesisVoice
+    synth.speak(sentenceToSpeak);
+  };
+
+  render() {
+    let {isListeningToUser, sentence, isExerciseCorrect, isExerciseWrong} = this.state;
+
+    return (
+      <div className="wrapper">
+        <header>Engly speaking demo</header>
+        <main>
+          <h1>Click the microphone and say:</h1>
+          <p className="sentence-wrapper">
+            <img src={play} className="play" alt="Play button"
+                 onClick={this.playSentence}/>
+            <b className="sentence">{sentence}</b>
+          </p>
+          <img
+            src={mic}
+            className={isListeningToUser ? 'mic mic__listening': 'mic'}
+            alt="Microphone icon"
+            onClick={this.listenToUser}
+          />
+          {
+            isExerciseCorrect &&
+            <img
+              className="success"
+              src={success}
+              alt="Success icon"
+            />
+          }
+          {
+            isExerciseWrong && !isExerciseCorrect &&
+            <img
+              className="success"
+              src={fail}
+              alt="Fail icon"
+            />
+          }
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
